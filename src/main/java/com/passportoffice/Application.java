61207d41e79f41f50235920522d05a73b374e6ca
azhibareva <anastasia.zhibareva@sperasoft.com>
2022-06-01 13:35:31 +0300

    Application class

---
+++ src/main/java/com/passportoffice/Application.java
@@ -0,0 +1,14 @@
+package com.passportoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.passportoffice", "com.passportoffice.api" , "com.passportoffice.configuration", "com.passportoffice.service", "com.passportoffice.test"})
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplication(Application.class).run(args);
+    }
+}
