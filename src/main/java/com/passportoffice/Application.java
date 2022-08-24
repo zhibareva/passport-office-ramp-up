ebb73e4aee32691ccc5c4adc9ce203fe73344f7b
zhibareva <zhibareva.a@gmail.com>
2022-08-24 13:09:35 +0300

    remove component scan from Application.java

--- src/main/java/com/passportoffice/Application.java
+++ src/main/java/com/passportoffice/Application.java
@@ -2,21 +2,11 @@ package com.passportoffice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {
-        "com.passportoffice",
-        "com.passportoffice.api",
-        "com.passportoffice.configuration",
-        "com.passportoffice.service",
-        "com.passportoffice.test",
-        "validation.validator"
-})
-
 public class Application {
 
-    public static void main(String[] args) {
-        new SpringApplication(Application.class).run(args);
-    }
+  public static void main(String[] args) {
+    new SpringApplication(Application.class).run(args);
+  }
 }
