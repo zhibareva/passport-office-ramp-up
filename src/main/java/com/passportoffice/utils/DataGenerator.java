d83b1306dc1aad2b0a23e84d8efffb81a6aa8798
zhibareva <zhibareva.a@gmail.com>
2022-08-24 13:20:42 +0300

    update data generator

--- src/main/java/com/passportoffice/utils/DataGenerator.java
+++ src/main/java/com/passportoffice/utils/DataGenerator.java
@@ -1,27 +1,26 @@
 package com.passportoffice.utils;
 
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-
 @Component
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DataGenerator {
 
-    public static Long generatePassportNumber() {
-        long leftLimit = 100000L;
-        long rightLimit = 999999L;
-        return leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
-    }
+  public static Long generatePassportNumber() {
+    long leftLimit = 100000L;
+    long rightLimit = 999999L;
+    return leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
+  }
 
-    public static LocalDate getCurrentDate() {
-        return LocalDate.now();
-    }
+  public static LocalDate getCurrentDate() {
+    return LocalDate.now();
+  }
 
-    public static String getName(String nameType) {
-        return nameType + RandomStringUtils.randomAlphabetic(10);
-    }
+  public static String getName(String nameType) {
+    return nameType + RandomStringUtils.randomAlphabetic(10);
+  }
 }
