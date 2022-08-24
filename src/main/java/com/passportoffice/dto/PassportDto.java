2ef590eae6763bda31ad1a4419b5512f197fcc63
zhibareva <zhibareva.a@gmail.com>
2022-08-24 13:18:02 +0300

    update service dtos

--- src/main/java/com/passportoffice/dto/PassportDto.java
+++ src/main/java/com/passportoffice/dto/PassportDto.java
@@ -2,45 +2,22 @@ package com.passportoffice.dto;
 
 import com.passportoffice.model.PassportType;
 import com.passportoffice.model.Status;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import validation.PassportId;
-
-import javax.validation.constraints.FutureOrPresent;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
+import javax.validation.constraints.FutureOrPresent;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 public class PassportDto {
 
-    @PassportId
-    @NotNull
-    @Min(value = 1)
-    private final Long passportId;
-
-    @NotNull
-    @Min(value = 1)
-    private final Long personId;
-
-    @NotNull
-    private final PassportType type;
-
-    @NotNull
-    private final Long number;
-
-    @PastOrPresent
-    private final LocalDate givenDate;
-
-    @FutureOrPresent
-    private final LocalDate expirationDate;
-
-    @NotNull
-    private final String departmentCode;
-
-    @NotNull
-    private final Status status;
-
+  private final String passportId;
+  private final PassportType type;
+  private final Long number;
+  private final LocalDate givenDate;
+  @FutureOrPresent private final LocalDate expirationDate;
+  private final String departmentCode;
+  private final Status status;
 }
--- src/main/java/com/passportoffice/dto/PersonDto.java
+++ src/main/java/com/passportoffice/dto/PersonDto.java
@@ -1,39 +1,17 @@
 package com.passportoffice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
-import validation.PersonId;
-
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 public class PersonDto {
-
-    @PersonId
-    @NotNull
-    private final Long id;
-
-    @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z\\s]+$")
-    private final String firstName;
-
-    @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z\\s]+$")
-    private final String lastName;
-
-    @Past
-    private final LocalDate dateOfBirth;
-
-    @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z\\s]+$")
-    private final String birthCountry;
-
+  private final String id;
+  private final String firstName;
+  private final String lastName;
+  private final LocalDate dateOfBirth;
+  private final String birthCountry;
 }
