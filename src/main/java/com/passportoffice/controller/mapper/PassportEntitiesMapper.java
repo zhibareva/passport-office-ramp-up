283b5e3a9c57a4174e93f2adfa94fd5b2757314f
zhibareva <zhibareva.a@gmail.com>
2022-09-21 16:50:17 +0300

    updated mappers

--- src/main/java/com/passportoffice/controller/mapper/PassportEntitiesMapper.java
+++ src/main/java/com/passportoffice/controller/mapper/PassportEntitiesMapper.java
@@ -1,6 +1,5 @@
 package com.passportoffice.controller.mapper;
 
-import com.passportoffice.dto.PassportDto;
 import com.passportoffice.dto.response.PassportResponse;
 import com.passportoffice.model.Passport;
 import org.mapstruct.Mapper;
@@ -10,18 +9,7 @@ import org.mapstruct.Mappings;
 @Mapper
 public interface PassportEntitiesMapper {
   @Mappings({
-    @Mapping(target = "id", source = "passportDto.passportId"),
-    @Mapping(target = "type", source = "passportDto.type"),
-    @Mapping(target = "number", source = "passportDto.number"),
-    @Mapping(target = "givenDate", source = "passportDto.givenDate"),
-    @Mapping(target = "expirationDate", source = "passportDto.expirationDate"),
-    @Mapping(target = "departmentCode", source = "passportDto.departmentCode"),
-    @Mapping(target = "status", source = "passportDto.status")
-  })
-  PassportResponse toResponse(PassportDto passportDto);
-
-  @Mappings({
-    @Mapping(target = "passportId", source = "passport.passportId"),
+    @Mapping(target = "id", source = "passport.passportId"),
     @Mapping(target = "type", source = "passport.type"),
     @Mapping(target = "number", source = "passport.number"),
     @Mapping(target = "givenDate", source = "passport.givenDate"),
@@ -29,5 +17,5 @@ public interface PassportEntitiesMapper {
     @Mapping(target = "departmentCode", source = "passport.departmentCode"),
     @Mapping(target = "status", source = "passport.status")
   })
-  PassportDto toDto(Passport passport);
+  PassportResponse toResponse(Passport passport);
 }
--- src/main/java/com/passportoffice/controller/mapper/PersonEntitiesMapper.java
+++ src/main/java/com/passportoffice/controller/mapper/PersonEntitiesMapper.java
@@ -1,6 +1,5 @@
 package com.passportoffice.controller.mapper;
 
-import com.passportoffice.dto.PersonDto;
 import com.passportoffice.dto.response.PersonResponse;
 import com.passportoffice.model.Person;
 import org.mapstruct.Mapper;
@@ -10,20 +9,11 @@ import org.mapstruct.Mappings;
 @Mapper
 public interface PersonEntitiesMapper {
   @Mappings({
-    @Mapping(target = "id", source = "personDto.id"),
-    @Mapping(target = "firstName", source = "personDto.firstName"),
-    @Mapping(target = "lastName", source = "personDto.lastName"),
-    @Mapping(target = "dateOfBirth", source = "personDto.dateOfBirth"),
-    @Mapping(target = "birthCountry", source = "personDto.birthCountry")
-  })
-  PersonResponse toResponse(PersonDto personDto);
-
-  @Mappings({
     @Mapping(target = "id", source = "person.id"),
     @Mapping(target = "firstName", source = "person.firstName"),
     @Mapping(target = "lastName", source = "person.lastName"),
     @Mapping(target = "dateOfBirth", source = "person.dateOfBirth"),
     @Mapping(target = "birthCountry", source = "person.birthCountry")
   })
-  PersonDto toDto(Person person);
+  PersonResponse toResponse(Person person);
 }
