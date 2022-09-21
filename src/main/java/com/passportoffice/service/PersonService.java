1bb1b4b7def400bdace2d85c345f1aa4aa23e3d0
zhibareva <zhibareva.a@gmail.com>
2022-09-21 16:48:10 +0300

    updated person service

--- src/main/java/com/passportoffice/service/PersonService.java
+++ src/main/java/com/passportoffice/service/PersonService.java
@@ -1,15 +1,16 @@
 package com.passportoffice.service;
 
-import com.passportoffice.dto.PersonDto;
-
+import com.passportoffice.model.Person;
 import java.time.LocalDate;
 
 public interface PersonService {
-    PersonDto getPerson(String id);
+  Person getPerson(String id);
 
-    PersonDto deletePerson(String id);
+  Person deletePerson(String id);
 
-    PersonDto updatePerson(String id, String firstName, String lastName, LocalDate dateOfBirth, String birthCountry);
+  Person updatePerson(
+      String id, String firstName, String lastName, LocalDate dateOfBirth, String birthCountry);
 
-    PersonDto createPerson(String firstName, String lastName, LocalDate dateOfBirth, String birthCountry);
+  Person createPerson(
+      String firstName, String lastName, LocalDate dateOfBirth, String birthCountry);
 }
--- src/main/java/com/passportoffice/service/impl/PersonServiceImpl.java
+++ src/main/java/com/passportoffice/service/impl/PersonServiceImpl.java
@@ -1,12 +1,10 @@
 package com.passportoffice.service.impl;
 
-import com.passportoffice.controller.mapper.PersonEntitiesMapper;
-import com.passportoffice.dto.PersonDto;
 import com.passportoffice.exception.PersonNotFoundException;
 import com.passportoffice.model.Person;
 import com.passportoffice.repository.PersonRepository;
-import com.devskiller.friendly_id.FriendlyId;
 import com.passportoffice.service.PersonService;
+import com.passportoffice.utils.IdGenerator;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
@@ -20,49 +18,43 @@ import org.springframework.stereotype.Service;
 public class PersonServiceImpl implements PersonService {
 
   private final PersonRepository personRepository;
-  private final PersonEntitiesMapper personEntitiesMapper;
+  private final IdGenerator idGenerator;
 
   @SneakyThrows
   @Override
   @NonNull
-  public PersonDto getPerson(String id) {
+  public Person getPerson(String id) {
     log.info("Searching for person with id [{}]", id);
-    Person person =
-        personRepository
-            .findById(id)
-            .orElseThrow(() -> new PersonNotFoundException("There is no person with such id"));
-    return personEntitiesMapper.toDto(person);
+    return personRepository
+        .findById(id)
+        .orElseThrow(() -> new PersonNotFoundException("There is no person with such id"));
   }
 
   @Override
   @NonNull
-  public PersonDto deletePerson(String id) {
+  public Person deletePerson(String id) {
     log.info("Deleting for person with id [{}]", id);
-    Person person =
-        personRepository
-            .deleteById(id)
-            .orElseThrow(() -> new PersonNotFoundException("There is no person with such id"));
-    return personEntitiesMapper.toDto(person);
+    return personRepository
+        .deleteById(id)
+        .orElseThrow(() -> new PersonNotFoundException("There is no person with such id"));
   }
 
   @Override
   @NonNull
-  public PersonDto updatePerson(
+  public Person updatePerson(
       String id, String firstName, String lastName, LocalDate dateOfBirth, String birthCountry) {
     Person personModel = new Person(id, firstName, lastName, dateOfBirth, birthCountry);
     log.info("Updating for person with id [{}] with data [{}]", id, personModel);
-    Person person = personRepository.save(id, personModel);
-    return personEntitiesMapper.toDto(person);
+    return personRepository.save(id, personModel);
   }
 
   @Override
   @NonNull
-  public PersonDto createPerson(
+  public Person createPerson(
       String firstName, String lastName, LocalDate dateOfBirth, String birthCountry) {
-    String personId = FriendlyId.createFriendlyId();
+    String personId = idGenerator.getId();
     Person personModel = new Person(personId, firstName, lastName, dateOfBirth, birthCountry);
-    log.info("Creating for person with id [{}] with data [{}]", personId, personModel.toString());
-    Person person = personRepository.save(personId, personModel);
-    return personEntitiesMapper.toDto(person);
+    log.info("Creating for person with id [{}] with data [{}]", personId, personModel);
+    return personRepository.save(personId, personModel);
   }
 }
